f9eee015af07d9e076e8fae04ee0c7a6d36d676f
abdullah107189 <abdullah107189@gmail.com>
2024-12-22 17:14:40 +0600

    my queries card delete api create

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fx40ttv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -42,7 +42,15 @@ async function run() {
             if (email) {
                 query = { authorEmail: email }
             }
-            const result = await queriesCollection.find(query).toArray()
+            const result = await queriesCollection.find(query).sort({ uploadDate: -1 }).toArray()
+            res.send(result)
+        })
+
+        // delete my queries single data 
+        app.delete('/my-querie/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await queriesCollection.deleteOne(query)
             res.send(result)
         })
 
